2d4bfc2f2ca039430396e50f84686fdf5f7176d0
ThanhBinh <binhthanh.vubc269@gmail.com>
2024-02-28 15:30:13 +0700

    by Binh Vu

--- app/src/main/java/com/example/sportify/MainActivity.java
+++ app/src/main/java/com/example/sportify/MainActivity.java
@@ -4,24 +4,30 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.AppCompatButton;
 
 import android.content.Intent;
+import android.media.AudioManager;
+import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ArrayList<BaiHat> danhSachBaiHat = new ArrayList<>();
     TextView tenbai,tentg;
     AppCompatButton playlist;
-    ImageView play1,play2,play3,play4;
+    ImageView play1,play2,play3,play4,play_pause,nextbai;
     int sttbai;
+    private MediaPlayer mp = new MediaPlayer();
+    private AudioManager audioManager;
+    private int currentPosition;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        ArrayList<BaiHat> danhSachBaiHat = new ArrayList<>();
 
         // Tạo và thêm các đối tượng BaiHat vào danh sách
         BaiHat baiHat1 = new BaiHat(1, "Ai ố xì mà", "Nguyễn Đức Phương", "ai_o_si_ma.mp3","img_1");
@@ -39,10 +45,51 @@ public class MainActivity extends AppCompatActivity {
         play2=findViewById(R.id.play2);
         play3=findViewById(R.id.play3);
         play4=findViewById(R.id.play4);
-
+        play_pause=findViewById(R.id.play);
+        play_pause.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                resumeAudio();
+                pauseAudio();
+            }
+        });
         play1.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
+
+                phatbai(1);
+                sttbai=1;
+            }
+        });
+        play2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                phatbai(2);
+                sttbai=2;
+            }
+        });
+        play3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                phatbai(3);
+                sttbai=3;
+            }
+        });
+        play4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                phatbai(4);
+                sttbai=4;
+            }
+        });
+        nextbai=findViewById(R.id.nextsong);
+        nextbai.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                nextbai1();
 
             }
         });
@@ -59,6 +106,71 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+    }
+    public void  nextbai1(){
+        if(sttbai<4){
+            phatbai(sttbai+1);
+            sttbai+=1;
+        }
+        else {
+            phatbai(1);
+            sttbai=1;
+        }
+    }
+    public void phatbai(int stt){
+//        Toast.makeText(MainActivity.this, "bài số"+sttbai, Toast.LENGTH_SHORT).show();
+        audioManager = (AudioManager) getSystemService(AUDIO_SERVICE);
+        tenbai.setText(danhSachBaiHat.get(stt-1).getTenBaiHat());
+        tentg.setText(" ("+danhSachBaiHat.get(stt-1).getCaSi()+")");
+        try {
+            mp.reset();
+            switch (stt){
+                case 1:{
+                    mp.setDataSource(getResources().openRawResourceFd(R.raw.ai_o_si_ma));
+                    break;
+                }
+                case 2:{
+                    mp.setDataSource(getResources().openRawResourceFd(R.raw.nang_tho));
+                    break;
+                }
+                case 3:{
+                    mp.setDataSource(getResources().openRawResourceFd(R.raw.thang_dien));
+                    break;
+                }
+                case 4:{
+                    mp.setDataSource(getResources().openRawResourceFd(R.raw.loi_nho));
+                    break;
+                }
+            }
+             // Sử dụng getResource() để lấy đường dẫn đúng
+            mp.prepare();
+            mp.start();
+
+            play_pause.setImageResource(R.drawable.baseline_pause_24);
+//            mp.setOnCompletionListener(new MediaPlayer.OnCompletionListener() {
+//                @Override
+//                public void onCompletion(MediaPlayer mp) {
+//                    mp.start(); // Bắt đầu phát lại khi kết thúc
+//                }
+//            });
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+    }
+    public void pauseAudio() {
+        if (mp != null && mp.isPlaying()) {
+            mp.pause();
+            currentPosition = mp.getCurrentPosition();
+            play_pause.setImageResource(R.drawable.play5);
+        }
+    }
+
+    public void resumeAudio() {
+        if (mp != null && !mp.isPlaying()) {
+            mp.seekTo(currentPosition);
+            mp.start();
+            play_pause.setImageResource(R.drawable.baseline_pause_24);
+        }
     }
 
 }
